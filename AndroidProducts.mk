9f77dfb5859f81a234f9ec32354569107e05e9a3
darknius20 <darknius21@gmail.com>
2022-09-12 07:18:16 +0000

    FLUID BRINGUP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The fluidOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_juice.mk
+    $(LOCAL_DIR)/fluid_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_juice-user \
-    spark_juice-userdebug \
-    spark_juice-eng
+    fluid_juice-user \
+    fluid_juice-userdebug \
+    fluid_juice-eng
--- fluid_juice.mk
+++ fluid_juice.mk
@@ -6,30 +6,26 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Spark stuff.
-WITH_GAPPS := true
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_GAPPS_ARCH := arm64
-
-#Build
-SPARK_BUILD_TYPE := OFFICIAL
-
-#Maintainer
-PRODUCT_PRODUCT_PROPERTIES += \
-   ro.spark.maintainer=Darknius \
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_juice
+PRODUCT_NAME := fluid_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Fluid flags
+FLUID_BUILD_TYPE := UNOFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.fluid.maintainer=Darknius \
+	ro.fluid.cpu=BENGAL
 
 # Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
@@ -40,4 +36,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
-
