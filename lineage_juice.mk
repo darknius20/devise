444de76e2858147a78775d86fcee857456875487
darklightnest <fadhilm293@gmail.com>
2022-09-23 03:27:13 +0000

    Revert "juice: include Pixel Launcher MOD"
    This reverts commit a047d9dd49dac91e0f61e8001a286bf1b4f19db6.

--- lineage_juice.mk
+++ lineage_juice.mk
@@ -40,8 +40,3 @@ PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Pixel Launcher MOD
-$(call inherit-product, vendor/PixelLauncher/PixelLauncher.mk)
-PIXEL_LAUNCHER_VARIANT := ammit
-ICONS_VARIANT := teamfilesicons
