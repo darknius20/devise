dd4d6c4e7de74a9d47a66c14cb4c27e1e6ad5dd3
darknius09 <darkniuslightness@gmail.com>
2022-09-28 15:35:01 +0000

    Pixel Blaster bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The sparkOS Project
+# Copyright (C) 2021 The aospOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_juice.mk
+    $(LOCAL_DIR)/aosp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_juice-user \
-    spark_juice-userdebug \
-    spark_juice-eng
+    aosp_juice-user \
+    aosp_juice-userdebug \
+    aosp_juice-eng
--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,9 +6,9 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Spark stuff.
+# Inherit some common aosp stuff.
 WITH_GAPPS := true
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
@@ -16,7 +16,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -24,12 +24,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-#Build
-SPARK_BUILD_TYPE := OFFICIAL
-
-#Maintainer
-PRODUCT_PRODUCT_PROPERTIES += \
-   ro.spark.maintainer=Darknius
 # Fingerprint
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 BUILD_DESCRIPTION := redfin-user 11 RQ3A.210905.001 7511028 release-keys
